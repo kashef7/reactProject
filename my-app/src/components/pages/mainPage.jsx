a9668727dd5a0df2c9fcdab6ff94ecc787893d60
kashef7 <11410120230477@stud.cu.edu.eg>
2025-01-23 05:26:40 +0200

    changed the main page to display a textarea to add secret and added a button to it but nothing works yet

--- my-app/src/components/pages/mainPage.jsx
+++ my-app/src/components/pages/mainPage.jsx
@@ -6,9 +6,14 @@ export default function MainPage() {
     const { username } = location.state || { username: 'Guest' };
 
     return (
-        <div>
+        <div className='container'>
+            <div className='secret'>
             <h1>Hello {username}</h1>
             <p>Welcome to the main page</p>
+            <textarea rows="4" cols="30" placeholder='say your secret' type='text' />
+            </div>
+
+            <button className='secretButton'>Submit secret</button>
         </div>
     );
 }
