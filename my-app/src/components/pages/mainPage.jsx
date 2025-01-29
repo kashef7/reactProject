4571f36d63b5623012b9d70a7a4242f4dc728601
kashef7 <11410120230477@stud.cu.edu.eg>
2025-01-29 21:25:38 +0200

    added few style features to the main page

--- my-app/src/components/pages/mainPage.jsx
+++ my-app/src/components/pages/mainPage.jsx
@@ -4,8 +4,9 @@ import { Link,useLocation,useNavigate } from 'react-router-dom';
 export default function MainPage() {
     const location = useLocation();
     const navigate = useNavigate();
+    const [check, setCheck] = useState(false);
+    const [msg, setMsg] = useState("");
     const { username  , id} = location.state || { username: 'Guest' };
-
     const [secret , setSecret] = useState('');
 
     function handleSecretChange(event){
@@ -19,6 +20,13 @@ export default function MainPage() {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({ secret: secret , id: id})
+        }).then(data =>{
+            setCheck(true);
+            setMsg("Secret submitted successfully");
+            setSecret('');
+            setTimeout(() => {
+                setCheck(false);
+            }, 3000);
         });
     };
 
@@ -32,7 +40,8 @@ export default function MainPage() {
             <h1>Hello {username}</h1>
             <p>Welcome to the main page</p>
             <p>Your secret id is {id}</p>
-            <textarea onChange={handleSecretChange} rows="4" cols="30" placeholder='say your secret' type='text' />
+            <textarea onChange={handleSecretChange} rows="4" cols="30" value={secret} placeholder='say your secret' type='text' />
+            {check && <p className='msg'>{msg}</p>}
             </div>
             <button onClick={submitSecret} className='secretButton'>Submit secret🤫</button>
             <button onClick={findSecret} className='secretButton'>Find secret🙈</button>
